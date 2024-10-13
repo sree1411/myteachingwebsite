41196dfb3743c6402836f3b5defdf7b735dac148
sreekanth <sreekanth>
2024-10-13 09:45:45 +0530

    updated the institute name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-light text-center text-lg-start">
     <div className="text-center p-3" style={{ backgroundColor: '#f8f9fa' }}>
       © 2024 Your Company Name:
-      <NavLink className="text-dark" href="https://www.ssinstitute.tech"> yourwebsite.com</NavLink>
+      <NavLink className="text-dark" href="https://www.ssinstitute.tech"> www.ssinstitute.tech </NavLink>
     </div>
   </footer>
   )
